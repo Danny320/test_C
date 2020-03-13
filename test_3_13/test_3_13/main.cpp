84d514b47ef033f4c22706ee5505c05d34d1715b
Danny320 <2261536364@qq.com>
2020-03-13 22:35:04 +0800

    testc

---
+++ test_3_13/test_3_13/main.cpp
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+//用函数的方法求两个数的最大值
+//int big_num(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	printf("请输入两个数的值：");
+//	scanf("%d%d", &a, &b);
+//	int n= big_num(a, b);
+//	printf("%d\n", n);
+//	system("pause");
+//	return 0;
+//}
+//**********************************************
+//关于字符串
+//int main()
+//{
+//	
+//	char arr[] = "abc";
+//	char arr1[] = { 'a', 'b', 'c' };
+//	printf("%s\n", arr);
+//	printf("%s\n", arr1);
+//	printf("%d\n",strlen(arr1));
+//	printf("c:\test\32\test.c");//\t水平制表符
+//	system("pause");
+//	return 0;
+//}
+//**********求两数和**************************************
+//int Add(int x, int y)
+//{
+//	int sum = 0;
+//	sum = x + y;
+//	return sum;
+//}
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf("%d%d", &num1, &num2);
+//	int ret = Add(num1, num2);
+//	printf("%d\n",ret);
+//	system("pause");
+//	return 0;
+//}
+//***************给数组赋值*******************************************
+//int main()
+//{
+//	int arr[] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 100; i++)
+//	{
+//		arr[i] = i+1;
+//	}
+//	for (i = 0; i < 100; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//}
+
