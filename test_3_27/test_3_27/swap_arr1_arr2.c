bda28e6ae542e5e4773f287c0c4ce68f11e0bb9e
Danny320 <2261536364@qq.com>
2020-03-31 23:07:32 +0800

    冒泡排序

--- test_3_27/test_3_27/swap_arr1_arr2.c
+++ test_3_27/test_3_27/swap_arr1_arr2.c
@@ -23,27 +23,50 @@
 //}
 
 
-
+//void Reverse(int arr1[], int arr2[])
+//{
+//	int i = 0;
+//		for (i = 0; i<10; i++)
+//		{
+//			int tmp = arr1[i];
+//			arr1[i] = arr2[i];
+//			arr2[i] = tmp;
+//		}
+//}
+//void Print(int arr1[], int arr2[],int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr1[i]);
+//
+//	}
+//	printf("\n");
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr2[i]);
+//
+//	}
+//
+//}
 //int main()
 //{
 //	int arr1[10] = { 0 };
 //	int arr2[10] = { 0 };
 //	int i = 0;
+//	int sz = sizeof(arr1) / sizeof(arr1[0]);
 //	printf("请输入10个数字:>");
 //	for (i = 0; i<10; i++)
 //	{
 //		scanf("%d", &arr1[i]);
 //	}
 //	printf("请输入10个数字:>");
+//
 //	for (i = 0; i<10; i++)
 //	{
 //		scanf("%d", &arr2[i]);
 //	}
-//	for (i = 0; i<10; i++)
-//	{
-//		int tmp = arr1[i];
-//		arr1[i] = arr2[i];
-//		arr2[i] = tmp;
-//	}
+//	Reverse(arr1, arr2);
+//	Print(arr1, arr2, sz);
 //	return 0;
 //}
