96b6d995351342965b2fce1998f66b0ebd305a50
Danny320 <2261536364@qq.com>
2020-04-05 23:59:14 +0800

    1

---
+++ test_4_5/test_4_5/test.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<assert.h>
+//int my_strlen(const char*str)
+//{
+//	int count = 0;
+//	assert(*str != NULL);
+//	while (*str!='\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//main()
+//{
+//	char arr[] = "abcdef";
+//	int len=my_strlen(arr);
+//	printf("%d\n", len);
+//	return 0;
+//}
