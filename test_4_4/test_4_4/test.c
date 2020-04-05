5d0fb63f957b5a84df9be30ed731f2ca2dedfc0a
Danny320 <2261536364@qq.com>
2020-04-05 23:56:38 +0800

    1

---
+++ test_4_4/test_4_4/test.c
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//typedef int* pint;
+//#define POINT int*
+//
+//int main()
+//{
+//	int a, b, c;//a b c均为整形
+//	int*pa, pb, pc;//pa为指针，pb，pc为整形；
+//	//pint pa, pb, pc;//都为指针
+//	//POINT pa, pb, pc;//pa为指针，pb，pc为整形；
+//	pb = &a;
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int i = 0;
+//	int ret = 1;
+//	int j = 0;
+//	int sum = 0;
+//	for (j = 1; j <= n; j++)
+//	{
+//		//计算j的阶乘
+//		ret = 1;
+//		for (i = 1; i <= j; i++)
+//		{
+//			ret *= i;
+//		}
+//		sum += ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int i = 0;
+//	int ret = 1;
+//	int j = 0;
+//	int sum = 0;
+//	for (j = 1; j <= n; j++)
+//	{
+//		ret *= j;
+//		sum += ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 10;
+//	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	for (i = 0; i <= 12; i++)
+//	{
+//		arr[i] = 0;
+//		printf("a\n");
+//	}
+//	return  0;
+//}
