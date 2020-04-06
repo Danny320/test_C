bf80437515279e4af195ec8d9db70c9828b9a2f1
Danny320 <2261536364@qq.com>
2020-04-06 23:55:09 +0800

    1

--- test_4_5/test_4_5/test.c
+++ test_4_5/test_4_5/test.c
@@ -19,3 +19,83 @@
 //	printf("%d\n", len);
 //	return 0;
 //}
+
+
+
+//模拟实现库函数strcpy
+//void my_strcpy(char*dest, char* src)
+//{
+//	while (*dest != '\0')
+//	{
+//		*dest = *src;
+//		dest++;
+//		src++;
+//	}
+//	*dest = *src;
+//}
+
+
+//优化
+//char* my_strcpy(char* dest, const char*src)
+//{
+//	char*ret = dest;
+//	assert(dest != NULL);
+//	assert(src != NULL);
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//	return ret;
+//}
+//
+//int main()
+//{
+//	char arr1[20] = "##################";
+//	const char arr2[] = "abcdef";
+//	char* ret = my_strcpy(arr1, arr2);
+//	printf("%s\n", ret);
+//	return 0;
+//}
+
+
+//调整奇数偶数顺序
+//void swap_arr(int arr[], int sz)
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//	int tmp = 0;
+//	while (left<right)
+//	{
+//		while ((left < right) && (arr[left]%2 != 0))
+//		{
+//			left++;
+//		}
+//		while ((left < right) && (arr[right] % 2 != 1))
+//		{
+//			right--;
+//		}
+//		if (left < right)
+//		{
+//			int tmp = arr[left];
+//			arr[left] = arr[right];
+//			arr[right] = tmp;
+//		}
+//	}
+//}
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//	}
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	swap_arr(arr,sz);
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
